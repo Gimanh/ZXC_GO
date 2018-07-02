77cd341dfa6624b189754b62a5f73a7f5714f109
nikolaygiman <gnl@MAIL.RU>
2018-07-02 23:51:09 +0300

    Main test )))

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package ZXC_GO
+package main
+
+import "fmt"
+
+func main() {
+	bob := Cats{"NIK", 7, 0.8988}
+	fmt.Println("Bob is", bob, "\n ", "Bob func is ", bob.test())
+	fmt.Println(bob)
+}
+
+type Cats struct {
+	name      string
+	age       int
+	happiness float64
+}
+
+func (cat *Cats) test() float64 {
+	cat.age = 123
+	return float64(cat.age) * cat.happiness
+}
